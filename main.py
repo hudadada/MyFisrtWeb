3e98183593bd40bfb57c4aa15e0623297ec93afa
hudada <13940156612@163.com>
2018-12-04 10:11:01 +0800

    数据库配置

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 app = Flask(__name__)
 
 class Config:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/myweb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+sr = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
 @app.route('/')
 def index():
