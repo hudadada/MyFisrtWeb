1c2307b25ba666b22bf4362cddb12da234f2616b
hudada <13940156612@163.com>
2018-12-05 15:26:44 +0800

    脚本启动与数据迁移

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
@@ -30,6 +32,12 @@ db = SQLAlchemy(app)
 sr = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 # 初始化session存储对象
 Session(app)
+# 创建脚本启动管理器
+mgr = Manager(app)
+#初始化迁移器
+Migrate(app,db)
+# 生成迁移命令
+mgr.add_command("mc",MigrateCommand)
 
 @app.route('/')
 def index():
@@ -37,4 +45,4 @@ def index():
     return 'index is show'
 
 if __name__ == '__main__':
-    app.run()
+    mgr.run()
