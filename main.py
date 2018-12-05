34d23eaae6e3f3de828147e957eec9ada16e8af2
hudada <13940156612@163.com>
2018-12-05 15:07:10 +0800

    配置基本配置

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+from datetime import timedelta
+
 from flask import Flask
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 
 app = Flask(__name__)
 
 class Config:
+    # 调试模式开启
     DEBUG = True
+    # mysql数据库
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/myfirstweb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # REDIS数据库
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
-
+    # 配置session 存储
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USER_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+#封装配置 从对象中加载配置
 app.config.from_object(Config)
+#创建数据库连接对象
 db = SQLAlchemy(app)
+# 创建redis连接对象
 sr = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+# 初始化session存储对象
+Session(app)
 
 @app.route('/')
 def index():
